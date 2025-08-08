d3c29efd34a83e5ba6cdf44bf60f7b7386d1a083
Murilo <murilosc2002@gmail.com>
2025-08-08 16:15:07 -0300

    Add .next/static/chunks/src_app_order_page_tsx_9148afbf._.js 1754680396171

---
+++ .next/static/chunks/src_app_order_page_tsx_9148afbf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_83ca4a4a._.js",
+  "static/chunks/src_aa62cd26._.js"
+],
+    source: "dynamic"
+});
